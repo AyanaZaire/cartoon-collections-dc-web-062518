5b73cbcca3fbb71b41f7b95df724fa198a311e5b
AyanaZaire <ayanazaire@gmail.com>
2018-06-14 20:57:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,9 @@
-names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
-def roll_call_dwarves(names)
-  puts names.each_with_index { |index, names| }
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
