c1cb8c88fe282c1a260e1af01931a921e699da00
AyanaZaire <ayanazaire@gmail.com>
2018-06-14 21:04:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,10 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese(potentially_cheesy_items)
+def find_the_cheese(cheesy_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  potentially_cheesy_items.find do |maybe_cheese|
+  cheesy_items.find do |maybe_cheese|
     cheese_types.include?(maybe_cheese)
   end
 end
